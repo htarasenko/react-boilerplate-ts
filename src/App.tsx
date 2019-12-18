4eea8972efbd1100827dfc5605e838c202047c19
Andrey Prisniak <aprisniak@lectrum.io>
2019-12-18 11:49:13 +0200

    feat(source): initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+export const App: React.FC = () => {
+  return (
+    <Button variant="contained" color="primary">
+      Push The Button
+    </Button>
+  );
+}
+
